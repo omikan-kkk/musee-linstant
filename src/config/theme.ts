ebc31dd1d99423e0363bfad9ce6b89454a1d8cd0
ryohei kaneda <azamisound@gmail.com>
2026-06-28 11:15:55 +0900

    Refine gallery exhibition layout

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -9,4 +9,23 @@ export const THEME = {
     workTitleSize: "10px",
     workBodySize: "10px",
   },
+
+  gallery: {
+    artworkAspectRatio: "4 / 3",
+    artworkMaxSize: "92%",
+    captionMarginTop: "14px",
+
+    maxWidth: "1200px",
+    mobileColumns: "2",
+    desktopColumns: "3",
+
+    mobileGapRow: "90px",
+    mobileGapColumn: "40px",
+
+    desktopGapRow: "110px",
+    desktopGapColumn: "48px",
+
+    mobilePadding: "120px 24px",
+    desktopPadding: "140px 32px",
+  },
 };
